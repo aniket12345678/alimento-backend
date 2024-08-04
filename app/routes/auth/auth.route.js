960f36f8690edabf05036e4a718361f46e49a369
aniket adak <aniketadak148@gmail.com>
2024-08-04 11:32:48 +0530

    issue resolved

--- app/routes/auth/auth.route.js
+++
@@ -1,8 +0,0 @@
-const auth_route = require('express')();
-
-const { signin, signup } = require('../../controllers/auth.controller');
-
-auth_route.post('/signin', signin);
-auth_route.post('/signup', signup);
-
-module.exports = { auth_route };
